f88c1017af9261df5d0318290c395adde9c7b961
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-05 15:47:42 +0000

    Validation check for banana quantity when building unit

--- BlazorBattles/BlazorBattles/Client/Pages/Build.razor.cs
+++ BlazorBattles/BlazorBattles/Client/Pages/Build.razor.cs
@@ -12,11 +12,17 @@ namespace BlazorBattles.Client.Pages
         public IUnitService Units { get; private set; }
 
         private int selectedUnitTemplateId = 1;
+        private bool notEnoughBananas = false;
 
 
         public void BuildUnit()
         {
             var unitTemplate = Units.FindTemplate(selectedUnitTemplateId);
+            notEnoughBananas = Bananas.Count < unitTemplate.Cost;
+
+            if (notEnoughBananas)
+                return;
+
             Bananas.Consume(unitTemplate.Cost);
             Units.AddInstance(unitTemplate.Id);
         }
