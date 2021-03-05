67167afa63762ec40a51001cecc06312b4a11e5b
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-05 13:51:30 +0000

    Button for consuming bananas in build page

---
+++ BlazorBattles/BlazorBattles/Client/Pages/Build.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorBattles.Client.Pages
+{
+    public partial class Build
+    {
+        private void ConsumeBananas(int amount)
+        {
+            Console.WriteLine($"{amount} bananas consumed.");
+        }
+    }
+}
