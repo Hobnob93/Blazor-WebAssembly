9941a5a4b3d5f5313d43dec02631c43748f8c90e
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2021-03-05 15:00:33 +0000

    Added army page which uses the Unit Service

---
+++ BlazorBattles/BlazorBattles/Client/Pages/Army.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBattles.Client.Interfaces;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBattles.Client.Pages
+{
+    public partial class Army
+    {
+        [Inject]
+        public IUnitService Units { get; private set; }
+    }
+}
